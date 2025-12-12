3d86cca0258720f04cb0037341abb0f7810ae186
LauritzPetersen <lauritzpetersen1@gmail.com>
2025-12-12 11:49:53 +0100

    Update FileManager.java

--- src/manager/FileManager.java
+++ src/manager/FileManager.java
@@ -7,27 +7,31 @@ import java.io.*;
 
 public class FileManager {
 
+    // sets default savefile name
     private static final String DEFAULT_SAVE_FILE = "inventory_save.dat";
 
+    // saves our inventory to our savefile
     public static void autoSave(Inventory inventory) {
         try{
-            saveToFile(inventory, DEFAULT_SAVE_FILE);
+            saveToFile(inventory, DEFAULT_SAVE_FILE); // calls saveToFile method
         } catch (FileHandlingException e) {
             System.out.println("Failed to save inventory: " + e.getMessage());
         }
     }
 
+    // creates a savefile object and loads data from it into our inventory
     public static void autoLoad(Inventory inventory) {
         File saveFile = new File(DEFAULT_SAVE_FILE);
         if (saveFile.exists()) {
             try {
-                loadFromFile(inventory, DEFAULT_SAVE_FILE);
+                loadFromFile(inventory, DEFAULT_SAVE_FILE); // calls loadFromFile method
             } catch (FileHandlingException e) {
                 System.out.println("Failed to load inventory: " + e.getMessage());
             }
         }
     }
 
+    // deletes our savefile data
     public static void deleteSaveFile() {
         File saveFile = new File(DEFAULT_SAVE_FILE);
         if (saveFile.exists()) {
@@ -35,9 +39,10 @@ public class FileManager {
         }
     }
 
+    // Method for saving inventory data to our savefile
     private static void saveToFile(Inventory inventory, String filename) throws FileHandlingException {
         InventoryData data = new InventoryData(inventory.getPlayer(), inventory.getItems());
-
+        // creates an InventoryData object and then uploads it to the savefile
         try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
             out.writeObject(data);
         }catch (IOException e) {
@@ -45,14 +50,17 @@ public class FileManager {
         }
     }
 
+
+    // Method for loading inventory data from our savefile
     private static void loadFromFile(Inventory inventory, String filename) throws FileHandlingException {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
             InventoryData data = (InventoryData) in.readObject();
+            // reads InventoryData object from our savefile
 
-            inventory.clear();
+            inventory.clear(); // clears inventory before loading savefile
 
             Player player = inventory.getPlayer();
-            player.reset();
+            player.reset();    // resets player level before loading savefile
             for (int i = 1; i < data.getPlayerLevel(); i++) {
                 player.levelUp();
             }
@@ -61,7 +69,7 @@ public class FileManager {
                 try {
                     inventory.addItem(item);
                 } catch (InventoryFullException | WeightLimitException e) {
-                    // Skip items that don't fit when loading
+                    // If inventory is full or the weight limit is exceeded during loading, skip the item
                 }
             }
         } catch (FileNotFoundException e) {
@@ -69,7 +77,7 @@ public class FileManager {
         } catch (IOException e) {
             throw new FileHandlingException("Error reading file: " + e.getMessage());
         } catch (ClassNotFoundException e) {
-            throw new FileHandlingException("Incompatible file format");
+            throw new FileHandlingException("Data format error: " + e.getMessage());
         }
     }
 }
